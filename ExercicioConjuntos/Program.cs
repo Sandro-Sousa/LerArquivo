f96400c462c39cb41685e604bbed9ae87e9c0f31
Sandro-Sousa <Sandro-Souza-oliveira@hotmail.com>
2020-05-30 13:33:00 -0300

    SintaxeParaLerArquivo

---
+++ ExercicioConjuntos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace SintaxeBasicaParaLerArquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path)) // abre o arquivo.
+                {
+                    while (!sr.EndOfStream) // enquanto não abrir o arquivo.
+                    {
+                        string line = sr.ReadLine(); //ler o que está no arquivo
+                        Console.WriteLine(line); // imprimir o arquivo.
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
